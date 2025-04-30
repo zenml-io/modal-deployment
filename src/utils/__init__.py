13a8cdf72d8bb1ded59c1b9e3aaf1361e8355f2e
marwan37 <marwan.ext@zenml.io>
2025-04-30 15:36:13 -0500

    remove app_prefix from configs: not needed

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -18,9 +18,10 @@
 from src.utils.yaml_config import (
     get_config,
     get_config_value,
+    get_merged_config_path,
 )
 
-from utils.model_utils import (
+from src.utils.model_utils import (
     find_model_version,
     get_python_version_from_metadata,
     get_model_architecture_from_metadata,
